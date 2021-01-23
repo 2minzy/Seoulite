2ec3909ad66bb4dcda64704f163f14dd12d023bc
Minji <minjilee0423@gmail.com>
2021-01-24 03:07:30 +0900

    Updated function

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,19 @@
+const axios = reuqire('axios');
+
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: 'Hello, Minji',
-  };
+  try {
+    const result = axios.get(
+      `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_PUBLIC_WIFI_API_KEY}/json/SebcPublicWifiEng/1/5/`
+    );
+
+    return {
+      statusCode: 200,
+      body: result.data,
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e,
+    };
+  }
 };
