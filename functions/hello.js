d12e9f6103864ddfa76457bfc42edbb7e6eb2b3f
Minji <minjilee0423@gmail.com>
2021-01-24 03:31:08 +0900

    Updated functions

--- functions/hello.js
+++ functions/hello.js
@@ -2,17 +2,13 @@ const axios = require('axios');
 
 exports.handler = async (event, context) => {
   try {
-    console.log(process.env.REACT_APP_PUBLIC_WIFI_API_KEY);
-
     const result = await axios.get(
       `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_PUBLIC_WIFI_API_KEY}/json/SebcPublicWifiEng/1/5/`
     );
 
-    console.log(result.data);
-
     return {
       statusCode: 200,
-      body: JSON.stringify(result),
+      body: JSON.stringify(result.data),
     };
   } catch (e) {
     return {
