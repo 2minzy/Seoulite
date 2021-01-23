c4cda9bdd9a14cd900c4d3665f7adce85c431cb8
Minji <minjilee0423@gmail.com>
2021-01-24 03:01:46 +0900

    Added functions

--- src/services/seoulWifiAPI.js
+++ src/services/seoulWifiAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const MAX_ROWS = 1000; // max request data per request
 
 export const fetchLocations = async (start = 1, end = MAX_ROWS) => {
-  const url = `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_PUBLIC_WIFI_API_KEY}/json/SebcPublicWifiEng/${start}/${end}/`;
+  const url = `https://openapi.seoul.go.kr:8088/${process.env.REACT_APP_PUBLIC_WIFI_API_KEY}/json/SebcPublicWifiEng/${start}/${end}/`;
   const response = await axios.get(url);
   const locations = response.data.SebcPublicWifiEng.row;
   const totalCount = response.data.SebcPublicWifiEng.list_total_count;
