07aeb98ff8c0fd3e3179b7c5285e338962c36260
Minji <minjilee0423@gmail.com>
2021-01-25 00:48:45 +0900

    Added wifi function

---
+++ functions/wifi.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+exports.handler = async (event, context) => {
+  const fetchLocations = async (start = 1, end = MAX_ROWS) => {
+    const url = `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_PUBLIC_WIFI_API_KEY}/json/SebcPublicWifiEng/${start}/${end}/`;
+    const response = await axios.get(url);
+    const locations = response.data.SebcPublicWifiEng.row;
+    const totalCount = response.data.SebcPublicWifiEng.list_total_count;
+
+    if (totalCount > end) {
+      const newStart = end + 1;
+      const newEnd = Math.min(end + MAX_ROWS, totalCount);
+      return (await fetchLocations(newStart, newEnd)).concat(locations);
+    } else {
+      return locations;
+    }
+  };
+
+  try {
+    const locations = fetchLocations();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(locations),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e,
+    };
+  }
+};
--- src/services/seoulWifiAPI.js
+++ src/services/seoulWifiAPI.js
@@ -3,17 +3,11 @@ import axios from 'axios';
 const MAX_ROWS = 1000; // max request data per request
 
 export const fetchLocations = async (start = 1, end = MAX_ROWS) => {
-  const url = `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_PUBLIC_WIFI_API_KEY}/json/SebcPublicWifiEng/${start}/${end}/`;
+  const url = `https://seoulite.netlify.app/.netlify/wifi`;
   const response = await axios.get(url);
-  const locations = response.data.SebcPublicWifiEng.row;
-  const totalCount = response.data.SebcPublicWifiEng.list_total_count;
-  // console.log({ locations, totalCount });
+  const locations = response.data;
 
-  if (totalCount > end) {
-    const newStart = end + 1;
-    const newEnd = Math.min(end + MAX_ROWS, totalCount);
-    return (await fetchLocations(newStart, newEnd)).concat(locations);
-  } else {
-    return locations;
-  }
+  console.log(locations);
+
+  return locations;
 };
